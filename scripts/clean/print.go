0eaeed4bbd53a5c44a4ba4e9f68382497ca4e898
Jackson Kurtz <jkurtz678@gmail.com>
2022-03-17 19:27:35 -0700

    update token sorting

--- scripts/clean/print.go
+++ scripts/clean/print.go
@@ -7,8 +7,17 @@ import (
 )
 
 func PrintTokenIDs() {
+	tagMap := make(map[string][]fstore.FirestoreToken)
+
 	tokenIterate(func(client *fstore.FirestoreClient, dt fstore.FirestoreToken) error {
-		fmt.Printf("name %40s - token_id %s\n", dt.Token.Name, dt.Token.TokenID)
+		tagMap[dt.Token.Tag] = append(tagMap[dt.Token.Tag], dt)
 		return nil
 	})
+
+	for tag, arr := range tagMap {
+		fmt.Printf("\n%s\n", tag)
+		for _, t := range arr {
+			fmt.Printf("name %40s - token_id %s\n", t.Token.Name, t.Token.Tag)
+		}
+	}
 }
