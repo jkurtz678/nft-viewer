1a513621bb40d6e76cfbc2622b5a8b3b52a8f821
Jackson Kurtz <jkurtz678@gmail.com>
2022-03-17 19:30:18 -0700

    fix token text

--- scripts/clean/print.go
+++ scripts/clean/print.go
@@ -17,7 +17,7 @@ func PrintTokenIDs() {
 	for tag, arr := range tagMap {
 		fmt.Printf("\n%s\n", tag)
 		for _, t := range arr {
-			fmt.Printf("name %40s - token_id %s\n", t.Token.Name, t.Token.Tag)
+			fmt.Printf("name %40s - token_id %s\n", t.Token.Name, t.Token.TokenID)
 		}
 	}
 }
